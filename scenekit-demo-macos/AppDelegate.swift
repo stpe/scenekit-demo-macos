68d999b573183d41ac3b4e9c3e70aeff8c293374
Stefan Pettersson <stefpet@gmail.com>
2023-11-14 15:52:22 +0100

    Initial Commit

---
+++ scenekit-demo-macos/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  scenekit-demo-macos
+//
+//  Created by Stefan Pettersson on 2023-11-14.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
